1424e40e5a17fda3d98ac79c1fb7cd324ba4d2f7
outcastveron <39554024+outcastveron@users.noreply.github.com>
2024-08-25 22:27:34 +0800

    Create tkty.py

---
+++ tkty.py
@@ -0,0 +1,85 @@
+import time
+import os
+import re
+import base64
+import datetime
+import requests
+import threading
+from queue import Queue
+from datetime import datetime
+
+
+#  获取远程体育直播源文件
+url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Cx4x/Cxxx/main/TKTY.m3u"
+response = requests.get(url)
+m3u_content = response.text
+
+# 移除第一行
+# m3u_content = m3u_content.split('\n', 1)[1]
+
+# 初始化变量
+group_name = ""
+channel_name = ""
+channel_link = ""
+output_dict = {}
+
+# 处理每两行为一组的情况
+for line in m3u_content.split('\n'):
+    if line.startswith("#EXTINF"):
+        # 获取 group-title 的值
+        group_name = line.split('group-title="')[1].split('"')[0]
+        
+        # 获取频道名
+        channel_name = line.split(',')[-1]
+    elif line.startswith("http"):
+        # 获取频道链接
+        channel_link = line
+        # 合并频道名和频道链接
+        combined_link = f"{channel_name},{channel_link}"
+
+        # 将组名作为键，合并链接作为值存储在字典中
+        if group_name not in output_dict:
+            output_dict[group_name] = []
+        output_dict[group_name].append(combined_link)
+
+# 将结果写入 sport.txt 文件
+with open("sport.txt", "w", encoding="utf-8") as output_file:
+    # 遍历字典，写入结果文件
+    for group_name, links in output_dict.items():
+        output_file.write(f"{group_name},#genre#\n")
+        for link in links:
+            output_file.write(f"{link}\n")
+
+
+def txt_to_m3u(input_file, output_file):
+    # 读取txt文件内容
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    # 打开m3u文件并写入内容
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('#EXTM3U\n')
+
+        # 初始化genre变量
+        genre = ''
+
+        # 遍历txt文件内容
+        for line in lines:
+            line = line.strip()
+            if "," in line:  # 防止文件里面缺失“,”号报错
+                # if line:
+                # 检查是否是genre行
+                channel_name, channel_url = line.split(',', 1)
+                if channel_url == '#genre#':
+                    genre = channel_name
+                    print(genre)
+                else:
+                    # 将频道信息写入m3u文件
+                    f.write(f'#EXTINF:-1 group-title="{genre}",{channel_name}\n')
+                    f.write(f'{channel_url}\n')
+
+
+# 将txt文件转换为m3u文件
+txt_to_m3u('sport.txt', 'sport.m3u')
+
+print("任务运行完毕，分类频道列表可查看文件夹内iptv_list.txt和iptv_list.m3u文件！")
